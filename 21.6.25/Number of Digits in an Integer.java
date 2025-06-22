ef51158b5543d81943efbfc963b9d4097aa2db1e
Subhaharini05 <151053886+Subhaharini05@users.noreply.github.com>
2025-06-22 21:25:53 +0530

    Create Number of Digits in an Integer.java

---
+++ 21.6.25/Number of Digits in an Integer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), c = 0;
+        while(n > 0) {
+            c++;
+            n /= 10;
+        }
+        System.out.println(c);
+    }
+}
